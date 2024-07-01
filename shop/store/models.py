bde469f277459581c3b19f0fefebb67ff1456d46
aibekov nurkerim <aibekovnurkerim@gmai.com>
2024-07-01 16:41:01 +0600

    Модифицировал код

--- shop/store/models.py
+++ shop/store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    added_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время добавления")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость")
+    image = models.ImageField(upload_to='products/', verbose_name="Изображение")
+
+    def __str__(self):
+        return self.name
--- shop/store/views.py
+++ shop/store/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Category
+from .forms import CategoryForm, ProductForm
 
-# Create your views here.
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'store/products.html', {'products': products})
+
+def product_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+def categories_view(request):
+    categories = Category.objects.all()
+    return render(request, 'store/categories.html', {'categories': categories})
+
+def category_add_view(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories_view')
+    else:
+        form = CategoryForm()
+    return render(request, 'store/category_add.html', {'form': form})
+
+def category_edit_view(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories_view')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'store/category_edit.html', {'form': form, 'category': category})
+
+def category_delete_view(request, id):
+    category = get_object_or_404(Category, id=id)
+    category.delete()
+    return redirect('categories_view')
+
+def product_add_view(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_view', id=product.id)
+    else:
+        form = ProductForm()
+    return render(request, 'store/product_add.html', {'form': form})
+
+def product_edit_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_view', id=product.id)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'store/product_edit.html', {'form': form, 'product': product})
+
+def product_delete_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    product.delete()
+    return redirect('products_view')
